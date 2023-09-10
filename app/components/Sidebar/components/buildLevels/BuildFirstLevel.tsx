41f63918187cd40de008b01f66a71b1712100037
Iliya Brook <iliyabrook1987@gmail.com>
2023-09-10 18:47:53 +0300

    2

--- app/components/Sidebar/components/buildLevels/BuildFirstLevel.tsx
+++ app/components/Sidebar/components/buildLevels/BuildFirstLevel.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import {BuildSecondLevel} from "@/app/components/Sidebar/components/buildLevels/BuildSecondLevel";
 import {firstLevelMenu} from "@/app/components/Sidebar/data";
 import styles from './BuildLevels.module.scss';
+import { motion } from 'framer-motion';
 
 export const BuildFirstLevel = ({pathname}: { pathname: string }) => {
     return (
